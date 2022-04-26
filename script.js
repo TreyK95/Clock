ff3c66921a881d6f10d3ca879e9f3500e493d630
Trey Kemp <kemptrey@outlook.com>
2022-04-25 21:11:43 -0600

    dark mode done

--- script.js
+++ script.js
@@ -29,10 +29,13 @@ const months = [
   "Dec",
 ];
 
-
-toggle.addEventListener('click', (e) => {
-  const html = document.querySelectorAll('html')
-  if(html.classList.contains('dark')) {
-    html.classList.remove('dark') e.target.innerHTML = 'Dark Mode'
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark Mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
   }
-})
+});
